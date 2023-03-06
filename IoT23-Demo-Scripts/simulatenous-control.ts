ddd0b3393967eef6641d91f338d4d6a77dc88e32
Ege Korkan <egekorkan@gmail.com>
2023-03-06 20:49:33 +0100

    fix ip addresses

--- IoT23-Demo-Scripts/simulatenous-control.ts
+++ IoT23-Demo-Scripts/simulatenous-control.ts
@@ -16,15 +16,15 @@ const P1 = {
 
 // const uarmURL_V = "http://192.168.48.73:9000/virtualuarm";
 const uarmTD_V = JSON.parse(readFileSync("virtual_things_description/virtual_robot/virtual_uarm.td.json","utf-8"))
-const conveyor1URL_V = "http://192.168.48.73:9000/virtualconveyor1";
+const conveyor1TD_V = JSON.parse(readFileSync("virtual_things_description/virtual_conveyorbelt/virtual_conveyor_left.td.json","utf-8"))
 const uarmTD_R = JSON.parse(readFileSync("Real-Devices-TDs/Uarm.json","utf-8"))
 const conveyor1TD_R = JSON.parse(readFileSync("Real-Devices-TDs/ConveyorBelt1.json","utf-8"))
-
 
 main()
 
 async function main() {
     let Consumer = new Servient();
+
     Consumer.addCredentials({
         "urn:dev:ops:32473-ConveyorBelt-001": {
             username: "admin",
@@ -36,23 +36,23 @@ async function main() {
         }
     })
     Consumer.addClientFactory(new HttpClientFactory());
-    Consumer.addClientFactory(new HttpsClientFactory());
+    Consumer.addClientFactory(new HttpsClientFactory({allowSelfSigned:true}));
     const wotHelper = new Helpers(Consumer);
 
     const WoT = await Consumer.start();
-    console.log("test0")
-    // const conveyor1TD_V:any = await wotHelper.fetch(conveyor1URL_V)
-    // const uarmTD_V:any = await wotHelper.fetch(uarmURL_V)
-    // console.log("test1")
     let uarm_V = await WoT.consume(uarmTD_V)
     let uarm_R = await WoT.consume(uarmTD_R)
-    // let conveyor1_V = await WoT.consume(conveyor1TD_V)
+    let conveyor1_V = await WoT.consume(conveyor1TD_V)
     let conveyor1_R = await WoT.consume(conveyor1TD_R)
 
-    // await delay(3000)
+    await delay(3000)
     uarm_V.invokeAction("gripOpen");
     uarm_R.invokeAction("gripOpen");
-    // await uarm_V.invokeAction("gripClose");
+
+    await delay(3000)
+    uarm_R.invokeAction("gripClose");
+    uarm_V.invokeAction("gripClose");
+
 
     // await uarm_V.invokeAction("goTo",P1);
 
