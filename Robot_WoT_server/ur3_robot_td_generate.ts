e5f2b53a8cd36ca844fed709a7e02954dcb97d31
Zucheng Han <zucheng.han@tum.de>
2023-05-07 20:42:15 +0200

    add options input

--- Robot_WoT_server/ur3_robot_td_generate.ts
+++ Robot_WoT_server/ur3_robot_td_generate.ts
@@ -13,7 +13,18 @@ async function main() {
     let robotInfo = await rdg.robotInfogeneration(robotName,rootFolder); // just use coordinates in coppeliasim scene
     console.log(robotInfo);
 
-    await rdg.generateTD(robotName, rootFolder); 
+    let options = {
+        "unit of distance": "meter",
+        "description": "The template TD is consumed by WoT server for robotic digital twins",
+        "stlFilepath": "http://localhost:4000/ur3_robot/ur3_robot_shape.stl",
+        "csvFilepath": "http://localhost:4000/ur3_robot/ur3_robot_data_point.csv",
+        "sceneFilepath": "http://localhost:4000/ur3_robot/robot_digital_twins.ttt",
+        "href":"http://localhost:8081/",
+        "port": 8081
+
+    }
+
+    await rdg.generateTD(robotName, rootFolder,options); 
 
 
 }
