543614b82eb619372ce0018a898029d756f6b611
Zucheng Han <zucheng.han@tum.de>
2023-05-10 16:38:01 +0200

    add options

--- UR10_TD_Verification/UR10_TD_generation.ts
+++ UR10_TD_Verification/UR10_TD_generation.ts
@@ -14,7 +14,7 @@ async function main() {
     let sceneAddress = rootAddress + "/UR10_TD_verification.ttt";
     console.log(sceneAddress);
 
-    let filePath = rootAddress;
+    let filePath = rootAddress + "/UR10_folder";
     let robotName = "UR10";
 
     let rdg = new robotDescriptiongenrate(sceneAddress);
@@ -29,8 +29,19 @@ async function main() {
     let robotInfo = await rdg.robotInfogeneration(robotName,filePath,posOutput,posInput);
     console.log(robotInfo);
 
+    let options = {
+        "unit of distance": "millimeter",
+        "description": "The TD for the virtual ur10 robot",
+        "stlFilepath": "http://localhost:4000/ur10_robot/UR10_shape.stl",
+        "csvFilepath": "http://localhost:4000/ur10_robot/UR10_data_point.csv",
+        "sceneFilepath": "http://localhost:4000/ur10_robot/UR10_TD_verification.ttt",
+        "href":"http://localhost:8001/",
+        "port": 8001
+    }
+
+
     let robotInfofolder = rootAddress + "/UR10_folder";
-    let result = await rdg.generateTD(robotName, robotInfofolder); 
+    let result = await rdg.generateTD(robotName, robotInfofolder, options); 
 
     console.log(result);
     
