21900ae82348c89e31564067f1699c5707a95aff
Zucheng <ge96zet@mytum.de>
2023-07-08 10:20:40 +0200

    add workflow for mycobot

---
+++ URDF_to_Robwot/mycobot_Robwot.ts
@@ -0,0 +1,98 @@
+const {RemoteAPIClient} = require("./remoteApi/RemoteAPIClient.js");
+var path = require('path');   // for root path
+
+// for class load urdf
+import {robotURDFload} from "../Load_URDF_robot/load_urdf_class";
+
+// for class robot description generation
+import {robotDescriptiongenrate} from "../Generate_robot_description/robot_description_generation_class";
+
+// for class to generate the robot wot server
+import {robotWoTserver} from "../Robot_WoT_server/WoT_server_generation_class";
+
+
+// add delay function
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
+async function robotModelGen() {
+    let rootAddress = path.resolve(__dirname, '..'); // get the root directory of the repository
+
+    let sceneAddress = rootAddress + "/Coppeliasim scene/robot_urdf_load_scene.ttt";
+    console.log(sceneAddress);
+    let fileAddress = rootAddress + "/Load_URDF_robot/URDF example/mycobot_description/urdf/mycobot.urdf";
+    let robotName = "mycobot280";
+    let robURDF = new robotURDFload(sceneAddress,fileAddress,robotName);
+
+    // first generate the robot model based on the URDF file
+    await robURDF.loadURDF();
+}
+
+async function robotTDGen() {
+    let rootAddress = path.resolve(__dirname, '..'); // get the root directory of the repository
+    let dtSceneAddress = rootAddress + "/URDF_to_Robwot/mycobot_simple_scene.ttt";
+
+    let rdg = new robotDescriptiongenrate(dtSceneAddress);
+
+    // load the robot model to the scene
+    let modelAddress = rootAddress + "/Coppeliasim scene/default robot models/mycobot280.ttm";
+    // set the position of model in the scene
+    let robotName = await rdg.loadModel(modelAddress,[0,0,0.01]); 
+    console.log(robotName);
+
+    // generate the robot info and TD
+    let rootFolderPath = rootAddress + "/URDF_to_Robwot/robot_info/mycobot_robot"; //the path of folder to save necessary files
+
+    // generate related info based on robot model in coppeliasim
+    let robotInfo = await rdg.robotInfogeneration(robotName, rootFolderPath);
+    console.log(robotInfo);
+
+    let optionsMypal = {
+        "unit of distance": "meter",
+        "stlFilepath": "http://localhost:4000/mycobot_robot/mycobot_robot_shape.stl",
+        "csvFilepath": "http://localhost:4000/mycobot_robot/mycobot_robot_data_point.csv",
+        "sceneFilepath": "http://localhost:4000/mycobot_robot/mycobot_robot.ttm",
+        "href":"http://localhost:8091/",
+        "port": 8091
+    }
+
+    // generate TD file base on robot name and necessary files in folder
+    await rdg.generateTD(robotName, rootFolderPath,optionsMypal);  
+}
+
+async function robotWoTGen() {
+    let rootAddress = path.resolve(__dirname, '..'); // get the root directory of the repository
+    let dtSceneAddress = rootAddress + "/URDF_to_Robwot/mycobot_simple_scene.ttt";
+
+    let driverAddress = rootAddress + "/Robot_WoT_server/robot_driver.txt";
+
+    let shapePath = path.resolve(__dirname, '..') + "/URDF_to_Robwot/robot_info/mycobot_robot/mycobot280_shape.stl";
+    let pointPath = path.resolve(__dirname, '..') + "/URDF_to_Robwot/robot_info/mycobot_robot/mycobot280_data_point.csv";
+    let robotTD = rootAddress + "/URDF_to_Robwot/robot_info/mycobot_robot/mycobot280_instance.json";
+
+    let robotName = "mycobot280";
+
+    let mycobot_robot = new robotWoTserver(dtSceneAddress,driverAddress,robotTD,robotName,null,null,null,shapePath,pointPath); // it requires coppeliaism
+
+    //initial the virtual robot WoT server
+    await mycobot_robot.serverInit();
+
+    await delay(3000);
+
+    
+}
+async function main() {
+
+    // load URDF file to generate robot model
+    await robotModelGen();
+
+    // based on robot model and correponding scene to generate TD
+    await robotTDGen();
+
+    // generate robot WoT server based on TD
+    await robotWoTGen();
+
+}
+
+main();
---
+++ URDF_to_Robwot/mycobot_WoT_client_template.ts
@@ -0,0 +1,41 @@
+const {RemoteAPIClient} = require("./remoteApi/RemoteAPIClient.js");
+var path = require('path');   // for root path
+
+// for class wot client
+import {makeWoTinteraction} from "../virtual_devices_WoT/clientClass";
+
+
+// add delay function
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
+
+async function main() {
+    let mycobot_URL = "http://localhost:8081/coppeliasim_virtualrobot_mycobot280";
+    let mycobot280 = new makeWoTinteraction(mycobot_URL);
+
+    let res1 = await mycobot280.readProperty("getCartesianposition");
+    console.log(res1);
+
+    let res2 = await mycobot280.readProperty("getJointposition");
+    console.log(res2);
+
+    
+    await mycobot280.invokeAction("moveTocartesianPosition", {"x":-0.125,"y":0.225,"z":0.29});
+
+    await delay(5000);
+
+    let res4 = await mycobot280.readProperty("getCartesianposition");
+    console.log(res4);
+
+    //await mycobot280.invokeAction("moveTojointPosition",{"joint1":0,"joint2":-30,"joint3":40,"joint4":135,"joint5":20,"joint6":0});
+    //await delay(8000);
+
+    let res5 = await mycobot280.readProperty("getJointposition");
+    console.log(res5);
+    
+
+}
+
+main();
