6a6e9ce8512375c8aa4c241f29c7146bbd6611b1
Ege Korkan <egekorkan@gmail.com>
2023-11-16 10:50:21 +0100

    rearrange light testing folder

---
+++ Setups/LightTesting/Readme.md
@@ -0,0 +1,8 @@
+## Light Testing
+
+This scene is about testing very simple lights in CoppeliaSim.
+You can change their colors.
+
+## Virtual light devices in CoppeliaSim
+
+<img src="./scene1 virtual light.JPG" width="400">
--- Setups/SimulationInTheLoop/Uarm_TD_Verification/uarm_TD_generation_template.ts
+++ Setups/SimulationInTheLoop/Uarm_TD_Verification/uarm_TD_generation_template.ts
@@ -33,62 +33,6 @@ async function main() {
     let sceneAddress:string = virtualSceneadress; 
 
     let rdg = new RobotDescriptionGenerator(sceneAddress);
-
-
-    // for dobot
-    /*
-    let robotName = "dobot";
-    let rootFolder = __dirname + "/robot_info/dobot_folder"; //the path of folder to save necessary files
-
-    let options = {
-        "unit of distance": "meter",
-        "description": "things description for dobot in current coppeliasim scene",
-        "stlFilepath": "http://localhost:4000/uarm_robot/dobot_shape.stl",
-        "csvFilepath": "http://localhost:4000/uarm_robot/dobot_data_point.csv",
-        "sceneFilepath": "http://localhost:4000/uarm_robot/Virtual_IoT_lab_verification.ttt",
-        "href":"http://localhost:8090/",
-        "port": 8090
-    };
-
-    await rdg.generateTD(robotName,rootFolder,options);
-    */
-
-
-    // uarm
-    /*
-    let robotName = "uarm";
-    let rootFolder = __dirname + "/robot_info/uarm_folder"; //the path of folder to save necessary files
-
-    let options = {
-        "unit of distance": "millimeter",
-        "description": "things description for uarm in current coppeliasim scene",
-        "stlFilepath": "http://localhost:4000/uarm_robot/uarm_shape.stl",
-        "csvFilepath": "http://localhost:4000/uarm_robot/uarm_data_point.csv",
-        "sceneFilepath": "http://localhost:4000/uarm_robot/Virtual_IoT_lab_verification.ttt",
-        "href":"http://localhost:8090/",
-        "port": 8090
-    }
-
-    await rdg.generateTD(robotName, rootFolder, options); 
-    */
-
-
-    /*
-    let sim = await init(sceneAddress);
-    let name = "/uarm";
-    let curHandle = Number(await sim.getObject(name));
-    let scriptHandle = Number(await sim.getScript(1, curHandle, name));
-    let robotInfo = await (sim.callScriptFunction("robotInfo", scriptHandle));
-    robotInfo = robotInfo[0];
-    console.log(robotInfo);
-    robotInfo = JSON.stringify(robotInfo);
-    await sim.stopSimulation();
-    await delay(200);
-    let jsonPath = "D:/master_thesis/project/robwot/Uarm_TD_Verification/robot_info/uarm_folder/uarm_info.json";
-    fs.writeFileSync(jsonPath, robotInfo);
-    */
-    
-    
 }
 
 main();
